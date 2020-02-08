76b5f6bd5578eba89cbb37eccb1a94f98fc7e67e
Kumar Gupta, Krishna (CT RDA DS AA TEC AU APPS) <krishnakumar.gupta@siemens.com>
2020-02-08 22:54:02 +0530

    added authentication

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import Home from './Home';
+import Secret from './Secret';
+import Login from './Login';
+import withAuth from './withAuth';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <Router>
+      <div>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/secret">Secret</Link></li>
+          <li><Link to="/login">Login</Link></li>
+        </ul>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/secret" component={withAuth(Secret)} />
+          <Route path="/login" component={Login} />
+        </Switch>
+      </div>
+      </Router>
+    );
+  }
 }
-
-export default App;
